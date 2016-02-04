e8579a8fdab0ff0f034742df818da1f58684d31f
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-02-04 12:53:14 -0500

    added getter methods for products

--- app/Product.php
+++ app/Product.php
@@ -9,4 +9,44 @@ class Product extends Model
 	protected $table = "products";
 	protected $fillable = ["name", "description", "img", "price", "quantity", "tags", "notes"];
 	protected $hidden = ["id", "notes"];
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getDescription()
+	{
+		return $this->description;
+	}
+
+	public function getImageName()
+	{
+		return $this->img;
+	}
+
+	public function getImage()
+	{
+		// TODO: return actual image
+	}
+
+	public function getPrice()
+	{
+		return $this->price;
+	}
+
+	public function getQuantity()
+	{
+		return $this->quantity;
+	}
+
+	public function getTags()
+	{
+		return $this->tags;
+	}
+
+	public function getNotes()
+	{
+		return $this->notes;
+	}
 }
